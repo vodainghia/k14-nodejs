a51cd05edb6f57c4d81c3b38a172007c44e2a31b
NghiaVo <nghiadaivo@gmail.com>
2024-01-14 16:48:22 +0700

    init commit

---
+++ lesson_1/Hello.js
@@ -0,0 +1,1 @@
+console.log("Hello, tui ten Teo!");
---
+++ lesson_1/VariableLearning.js
@@ -0,0 +1,7 @@
+let myName = 'Teo';
+myName = 18;
+myName = true;
+myName = null;
+myName = undefined;
+
+console.log(Infinity > Number.MAX_VALUE);

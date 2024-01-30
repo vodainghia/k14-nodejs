8d4ab68b5fdebef058984c67b017169e6d4e2426
NghiaVo <nghiadaivo@gmail.com>
2024-01-30 23:12:39 +0700

    re-implement Lab_3_2

--- labs/lab_3/Lab_3_2.js
+++ labs/lab_3/Lab_3_2.js
@@ -1,7 +1,12 @@
 // Finding maximum value/minimum value from an integer array
+const { findMinNumberInArray, findMaxNumberInArray } = require('../../lessons/utils/ArrayHelper');
 
-const intArr = [1, 2, 3, 4, 10, 6, 7, 8, 3];
+const intArr = [1, 3, 5, 3, 2, 1, 6, 9, 10, 13, 7];
 
-console.log("The max value in the integer array is: ", intArr.reduce((a, b) => Math.max(a, b)));
+// to compare results
+console.log(intArr.reduce((a, b) => Math.max(a, b)));
+console.log(intArr.reduce((a, b) => Math.min(a, b)));
 
-console.log("The min value in the integer array is: ", intArr.reduce((a, b) => Math.min(a, b)));
+// Result
+console.log("The max value in the integer array is: ", findMaxNumberInArray(intArr));
+console.log("The min value in the integer array is: ", findMinNumberInArray(intArr));
--- lessons/utils/ArrayHelper.js
+++ lessons/utils/ArrayHelper.js
@@ -17,10 +17,30 @@ const sortNumberDESC = function (firstNum, secondNum) {
     return secondNum - firstNum;
 }
 
+const findMaxNumberInArray = (arr) => {
+    let max = arr.length != 0 ? arr[0] : 0;
+    for (const num of arr) {
+        max = num > max ? num : max;
+    };
+
+    return max;
+}
+
+const findMinNumberInArray = (arr) => {
+    let min = arr.length != 0 ? arr[0] : 0;
+    for (const num of arr) {
+        min = num < min ? num : min;
+    };
+
+    return min;
+}
+
 module.exports = {
     filterEvenNumbers: filterEvenNumbers,
     filterOddNumbers: filterOddNumbers,
     convertToEvenNumbers: convertToEvenNumbers,
     sortNumberASC: sortNumberASC,
     sortNumberDESC: sortNumberDESC,
+    findMaxNumberInArray: findMaxNumberInArray,
+    findMinNumberInArray: findMinNumberInArray,
 }
